ab7a5abdc3506094cc0afbe7915936afcde23cfa
Ben Baker <benbaker@isti.com>
2016-01-19 15:21:41 -0800

    Initial run at least squares via lapack

--- c_src/utils.c
+++ c_src/utils.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+extern void dcopy_(int *n, double *x, int *incx, double *y, int *incy);
+extern void dgelsd_(int *M, int *N, int *NRHS, double *A, int *LDA,
+                    double *B, int *LDB, double *S, double *RCOND,
+                    int *RANK, double *work, int *lwork, int *iwork, int *info);
 /*!
  * @brief Computes the arithmetic mean of x where any NaN's are ignored
  * 
@@ -37,7 +42,118 @@ double numpy_nanmean(int n, double *x)
     }
     return xavg;
 }
+//============================================================================//
 
+int numpy_lstsq(int m, int n, int nrhs, double *Aref, double *b,
+                double *rcond_in, double *x, int *rank_out, double *svals)
+{
+    const char *fcnm = "numpy_lstsq\0";
+    double *A, *bwork, *work, *s, rcond, work8;
+    int *iwork, ierr, indx, jndx, info, iwork4, k,
+        lda, ldb, liwork, lwork, mn, rank;
+    int incx = 1, incy = 1;
+    // Initialize
+    A = NULL;
+    s = NULL;
+    work = NULL;
+    bwork = NULL;
+    iwork = NULL;
+    // Check sizes
+    ierr = 1;
+    if (m < 1){
+        printf("%s: Error no rows in matrix\n", fcnm);
+        goto ERROR; 
+    }
+    if (n < 1){
+        printf("%s: Error no columns in matrix\n", fcnm);
+        goto ERROR; 
+    }
+    if (nrhs < 1){
+        printf("%s: Error there are no right hand sides\n", fcnm);
+        goto ERROR; 
+    }
+    // Figure out the condition number
+    rcond =-1.0;
+    if (rcond_in != NULL){rcond = *rcond_in;}
+    // Set space for A
+    ierr = 2;
+    lda = m;
+    mn = lda*n;
+    A = (double *)calloc(mn, sizeof(double));
+    if (A == NULL){
+        printf("%s: There was an error setting space\n", fcnm);
+        goto ERROR; 
+    }
+    s = (double *)calloc(fmin(n, n), sizeof(double));
+    if (s == NULL){
+        printf("%s: ERror setting space for singular values\n", fcnm);
+        goto ERROR;
+    }
+    // Set space for right hand side/solution
+    ldb = fmax(m,n);
+    bwork = (double *)calloc(ldb*nrhs, sizeof(double));
+    if (bwork == NULL){
+        printf("%s: There was an error setting workspace for bwork\n", fcnm);
+        goto ERROR; 
+    }
+    // Copy RHS
+    dcopy_(&mn, Aref, &incx, A, &incy);
+    for (k=0; k<nrhs; k++){
+        indx = m*k;
+        jndx = ldb*k;
+        dcopy_(&m, &b[indx], &incx, &bwork[indx], &incy);
+    }
+    // Space inquiry and then set workspace
+    lwork =-1;
+    dgelsd_(&m, &n, &nrhs, A, &lda, bwork, &ldb, s, &rcond, &rank,
+            &work8, &lwork, &iwork4, &info);
+    if (info != 0){
+        printf("%s: There was an error in the space query!\n", fcnm);
+        return -1;
+    }
+    lwork = (int) work8;
+    liwork = iwork4;
+    work = (double *)calloc(lwork, sizeof(double));
+    iwork = (int *)calloc(liwork, sizeof(int));
+    if (work == NULL){
+        printf("%s: Error setting space for work\n", fcnm);
+        return 2;
+    }
+    if (iwork == NULL){
+        printf("%s: Error setting space for iwork\n", fcnm);
+        return 2;
+    }
+    // Compute the SVD
+    dgelsd_(&m, &n, &nrhs, A, &lda, bwork, &ldb, s, &rcond, &rank,
+            work, &lwork, iwork, &info);
+    if (info != 0){
+        printf("%s: There was an error solving the least squares problem\n",
+               fcnm);
+        goto ERROR;
+    }
+    // Copy the solution
+    for (k=0; k<nrhs; k++){
+        indx = k*ldb;
+        jndx = k*n;
+        dcopy_(&m, &bwork[indx], &incx, &x[jndx], &incy);
+    }
+    // Do you want the rank?
+    if (rank_out != NULL){*rank_out = rank;}
+    // Do you want the singular values?
+    if (svals != NULL){
+        mn = fmin(m, n);
+        dcopy_(&mn, s, &incx, svals, &incy);
+    }
+    // Free space
+ERROR:;
+    if (iwork != NULL){free(iwork);}
+    if (work  != NULL){free(work);}
+    if (bwork != NULL){free(bwork);}
+    if (s     != NULL){free(s);}
+    if (A     != NULL){free(A);}
+    return ierr;
+}
+//============================================================================//
 /*!
  * @brief Rotates horizontal components of a seismogram. 
  *        The north and east components of a seismogram will be rotated
