428ccafdbeda3f6b2b7385c9dcd3600c7206c209
Ben Baker <benbaker@isti.com>
2016-03-08 16:52:37 -0800

    Added a paraminit function name

--- src/include/gfast.h
+++ src/include/gfast.h
@@ -1,12 +1,18 @@
 #include "gfast_numpy.h"
 #include "gfast_obspy.h"
 #include "gfast_struct.h"
+#include "gfast_log.h"
+#include "gfast_os.h"
 #ifndef __GFAST__
 #define __GFAST__
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+/* Initializes the GFAST parameters */
+int GFAST_paraminit(char *propfilename, struct GFAST_props_struct *props);
+
+
 int GFAST_CMTgreenF(int l1, int ldg, 
                     double *x1, double *y1, double *z1, double *azi, 
                     double *G);
