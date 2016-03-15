bf49eb4bee3a3da85c64f36d2f073f796f77099b
Ben Baker <benbaker@isti.com>
2016-03-15 14:52:02 -0700

    Fixed a mistake in computing variance reduction

--- src/scaling.c
+++ src/scaling.c
@@ -7,7 +7,13 @@
 
 /*!
  * @brief Computes the predicted mangitude using PGD and Pd from
- *        the seismogeodetic data
+ *        the seismogeodetic data by solving the overdetermined system:
+ *         \f[ 
+ *            \left [B + C \log_{10}(r) \right ] \textbf{m}
+ *          = \log_{10}(d) - A
+ *         \f] 
+ *        for the the magnitude m.  Here r is the hypocentral distance,
+ *        d is the displacement, A, B, and C are defined in the routine. 
  *
  * @param[in] n         length of arrays 
  * @param[in] verbose   controls verbosity (0 is completely quiet)
@@ -25,6 +31,8 @@
  *
  * @author Brendan Crowell (PNSN) and Ben Baker (ISTI)
  *
+ * @addtogroup PGD
+ *
  */
 int GFAST_scaling_PGD(int n, int verbose,
                       double dist_tol, double dist_def,
@@ -33,7 +41,7 @@ int GFAST_scaling_PGD(int n, int verbose,
 {
     const char *fcnm = "GFAST_scaling_PGD\0";
     double *b, *G, *UP, *Wb, *WG, M1[1],
-           W, b10, repi_min, repi_min2, res;
+           W, b10, repi_min, repi_min2, res, xden, xnum;
     int i, ierr;
     const double A = -6.687;
     const double B = 1.500;
@@ -64,8 +72,8 @@ int GFAST_scaling_PGD(int n, int verbose,
     repi_min = numpy_min(n, repi);
     repi_min2 = pow(repi_min, 2);
     // Generate the over-determined system: [B + C*log10(r)]*m = log10(d) - A
-    G  = (double *)calloc(n, sizeof(double));
-    b  = (double *)calloc(n, sizeof(double));
+    G  = (double *)calloc(n*1, sizeof(double));
+    b  = (double *)calloc(n,   sizeof(double));
     ierr = GFAST_scaling_PGD__setForwardModel(n, verbose,
                                               B, C, r,
                                               G);
@@ -94,7 +102,7 @@ int GFAST_scaling_PGD(int n, int verbose,
         Wb[i] = W*b[i];
     }
     // Solve the weighted least squares problem (M = lstsq(W*G,W*b)[0])
-    ierr = numpy_lstsq(LAPACK_ROW_MAJOR, 
+    ierr = numpy_lstsq(LAPACK_COL_MAJOR, 
                        n, 1, 1, WG, Wb,
                        NULL, M1, NULL, NULL); 
     if (ierr != 0){
@@ -105,18 +113,20 @@ int GFAST_scaling_PGD(int n, int verbose,
     }
     // Compute the estimates: UP = G*M
     UP = (double *)calloc(n, sizeof(double));
-    cblas_dgemv(CblasRowMajor, CblasNoTrans,
+    cblas_dgemv(CblasColMajor, CblasNoTrans,
                 n, 1, 1.0, G, n, M1, 1, 0.0, UP, 1);
     // Compute the variance reduction
-    b10 = 0.0;
-    res = 0.0;
+    xnum = 0.0;
+    xden = 0.0;
     for (i=0; i<n; i++){
-        res = res + (pow(10.0, b[i]) - pow(10.0, UP[i]));
-        b10 = b10 + pow(10.0, b[i]);
+        res = pow(10.0, b[i]) - pow(10.0, UP[i]);
+        xnum = xnum + res*res;
+        b10 = pow(10.0, b[i]);
+        xden = xden + b10*b10;
     }
     // Copy results
     *M = M1[0];
-    *VR = ((1.0 - sqrt(res))/sqrt(b10))*100.0;
+    *VR = (1.0 - sqrt(xnum)/sqrt(xden))*100.0;
     // Free space
 ERROR:;
     if (b  != NULL){free(b);}
@@ -133,7 +143,10 @@ ERROR:;
 }
 //============================================================================//
 /*!
- * @brief Sets the forward modeling matrix G 
+ * @brief Sets the forward modeling matrix G s.t.
+ *        \f$ G = \left [ B + C \log_{10}(r) \right ] \f$
+ *        where B and C are scalar shifts and scale factors respectively
+ *        and r the hypocentral distances for all stations.
  *
  * @param[in] n        length of r and G (> 0)
  * @param[in] verbose  controls verbosity (0 is quiet)
@@ -141,10 +154,15 @@ ERROR:;
  * @param[in] C        scale factor in G = B + C*log10(r) 
  * @param[in] r        hypocentral distances (km) [n] 
  *
- * @param[out] G       forward modeling matrix for PGD [n x 1]
+ * @param[out] G       forward modeling matrix for PGD [n x 1].  Note that
+ *                     G is in column major format.
  *
  * @result 0 indicates success
  *
+ * @author Ben Baker (ISTI)
+ *
+ * @addtogroup PGD
+ * 
  */
 int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
                                        double B, double C,
@@ -167,7 +185,9 @@ int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
 //============================================================================//
 /*!
  * @brief Computes the right hand side in the peak ground displacement 
- *        estimation s.t. b = log10(d) - A 
+ *        estimation s.t.
+ *        \f$ \textbf{b} = \left \{ \log_{10}(d) - A \right \} \f$
+ *        where A is a scalar shift and d is the displacement at each station.
  *
  * @param[in] n         number of points
  * @param[in] verbose   controls verbosity (0 is quiet)
@@ -182,6 +202,8 @@ int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
  * @result 0 indicates success
  *
  * @author Ben Baker (ISTI)
+ *
+ * @addtogroup PGD
  *
  */
 int GFAST_scaling_PGD__setRHS(int n, int verbose,
