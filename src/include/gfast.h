187ef33c4d99c6b7beef8d1fdc6026821774987b
Ben Baker <benbaker@isti.com>
2016-03-15 10:31:07 -0700

    Adding scaling function

--- src/include/gfast.h
+++ src/include/gfast.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "gfast_numpy.h"
 #include "gfast_obspy.h"
 #include "gfast_struct.h"
@@ -10,6 +11,27 @@
 extern "C"
 {
 #endif
+/* Coordinate tools */
+void GFAST_coordtools_lla2ecef(double lat_in, double lon_in, double alt,
+                               double *x, double *y, double *z);
+void GFAST_coordtools_ecef2lla(double x, double y, double z,
+                               double *lat_deg, double *lon_deg, double *alt);
+void GFAST_coordtools_dxyz2dneu(double dx, double dy, double dz,
+                                double lat_deg, double lon_deg,
+                                double *dn, double *de, double *du);
+#ifdef WITH_LIBGEO
+int geodetic_ll2utm(double lat, double lon, double *xutm, double *yutm,
+                    bool *lnorthp, int *zone);
+int geodetic_utm2ll(int zone, bool lnorthp, double xutm, double yutm,
+                    double *lat, double *lon);
+#else
+void GFAST_coordtools_ll2utm(double lon_deg, double lat_deg, double lon0_deg,
+                             double *UTMNorthing, double *UTMEasting);
+void GFAST_coordtools_utm2ll(double UTMEasting, double UTMNorthing,
+                             double lon0_deg,
+                             double *lat_deg, double *lon_deg);
+#endif
+
 /* Initializes station locations/names */
 int GFAST_locinit(struct GFAST_props_struct props,
                   struct GFAST_data_struct *gps_data);
@@ -25,6 +47,19 @@ int GFAST_paraminit(char *propfilename, struct GFAST_props_struct *props);
 int GFAST_readElarmS(struct GFAST_props_struct props);
 int GFAST_readElarmS_ElarmSMessage2SAStruct(int verbose, char *buff,
                                             struct GFAST_shakeAlert_struct *SA);
+/* PGD Scaling */
+int GFAST_scaling_PGD(int n, int verbose,
+                      double dist_tol, double dist_def,
+                      double *d, double *r, double *repi,
+                      double *M, double *VR);
+
+int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
+                                       double B, double C, double *__restrict__ r,
+                                       double *__restrict__ G);
+int GFAST_scaling_PGD__setRHS(int n, int verbose,
+                              double dist_tol, double dist_def,
+                              double A, double *__restrict__ d,
+                              double *__restrict__ b);
 
 int GFAST_CMTgreenF(int l1, int ldg, 
                     double *x1, double *y1, double *z1, double *azi, 
---
+++ src/scaling.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <lapacke.h>
+#include <cblas.h>
+#include "gfast.h"
+
+/*!
+ * @brief Computes the predicted mangitude using PGD and Pd from
+ *        the seismogeodetic data
+ *
+ * @param[in] n         length of arrays 
+ * @param[in] verbose   controls verbosity (0 is completely quiet)
+ * @param[in] dist_tol  displacement tolerance (cm).  if the displacment
+ *                      is less than dist_tol it would be set to dist_tol 
+ * @param[in] dist_def  displacement default (cm) if d < dist_tol
+ * @param[in] d         displacement (cm) [n] 
+ * @param[in] r         hypocentral distance (km) [n]
+ * @param[in] repi      epicentral distance (km) [n]
+ *
+ * @param[out] M        magnitude
+ * @param[out] VR       variance reduction
+ *
+ * @result 0 indicates success
+ *
+ * @author Brendan Crowell (PNSN) and Ben Baker (ISTI)
+ *
+ */
+int GFAST_scaling_PGD(int n, int verbose,
+                      double dist_tol, double dist_def,
+                      double *d, double *r, double *repi,
+                      double *M, double *VR)
+{
+    const char *fcnm = "GFAST_scaling_PGD\0";
+    double *b, *G, *UP, *Wb, *WG, M1[1],
+           W, b10, repi_min, repi_min2, res;
+    int i, ierr;
+    const double A = -6.687;
+    const double B = 1.500;
+    const double C = -0.214;
+    //const double A = -4.434;
+    //const double B = 1.047;
+    //const double C = -0.138;
+    //------------------------------------------------------------------------//
+    //
+    // Initialize
+    ierr = 0;
+    *VR = 0.0;
+    *M = 0.0;
+    b = NULL;
+    G = NULL;
+    UP = NULL;
+    Wb = NULL;
+    WG = NULL;
+    // Error check
+    if (n < 1){
+        if (verbose > 0){
+            log_errorF("%s: Error invalid number of input stations: %d\n",
+                       fcnm, n);
+        }
+        goto ERROR;
+    }
+    // Get the min epicentral distance 
+    repi_min = numpy_min(n, repi);
+    repi_min2 = pow(repi_min, 2);
+    // Generate the over-determined system: [B + C*log10(r)]*m = log10(d) - A
+    G  = (double *)calloc(n, sizeof(double));
+    b  = (double *)calloc(n, sizeof(double));
+    ierr = GFAST_scaling_PGD__setForwardModel(n, verbose,
+                                              B, C, r,
+                                              G);
+    if (ierr != 0){
+        if (verbose > 0){
+            log_errorF("%s: Error creating G matrix\n", fcnm);
+        }
+        goto ERROR;
+    }
+    ierr = GFAST_scaling_PGD__setRHS(n, verbose,
+                                     dist_tol, dist_def,
+                                     A, d,
+                                     b);
+    if (ierr != 0){
+        if (verbose > 0){
+            log_errorF("%s: Error creating RHS\n", fcnm);
+        }
+        goto ERROR;
+    }
+    // Apply weights
+    WG = (double *)calloc(n, sizeof(double));
+    Wb = (double *)calloc(n, sizeof(double));
+    for (i=0; i<n; i++){
+        W = exp(-pow(repi[i], 2)/8.0/repi_min2); // Compute weights
+        WG[i] = W*G[i];
+        Wb[i] = W*b[i];
+    }
+    // Solve the weighted least squares problem (M = lstsq(W*G,W*b)[0])
+    ierr = numpy_lstsq(LAPACK_ROW_MAJOR, 
+                       n, 1, 1, WG, Wb,
+                       NULL, M1, NULL, NULL); 
+    if (ierr != 0){
+        if (verbose > 0){
+            log_errorF("%s: Error solving the least-squares problem\n", fcnm);
+        }
+        goto ERROR;
+    }
+    // Compute the estimates: UP = G*M
+    UP = (double *)calloc(n, sizeof(double));
+    cblas_dgemv(CblasRowMajor, CblasNoTrans,
+                n, 1, 1.0, G, n, M1, 1, 0.0, UP, 1);
+    // Compute the variance reduction
+    b10 = 0.0;
+    res = 0.0;
+    for (i=0; i<n; i++){
+        res = res + (pow(10.0, b[i]) - pow(10.0, UP[i]));
+        b10 = b10 + pow(10.0, b[i]);
+    }
+    // Copy results
+    *M = M1[0];
+    *VR = ((1.0 - sqrt(res))/sqrt(b10))*100.0;
+    // Free space
+ERROR:;
+    if (b  != NULL){free(b);}
+    if (G  != NULL){free(G);}
+    if (UP != NULL){free(UP);}
+    if (Wb != NULL){free(Wb);}
+    if (WG != NULL){free(WG);}
+    b  = NULL;
+    G  = NULL;
+    UP = NULL;
+    Wb = NULL;
+    WG = NULL;
+    return 0;
+}
+//============================================================================//
+/*!
+ * @brief Sets the forward modeling matrix G 
+ *
+ * @param[in] n        length of r and G (> 0)
+ * @param[in] verbose  controls verbosity (0 is quiet)
+ * @param[in] B        linear shift in G = B + C*log10(r)
+ * @param[in] C        scale factor in G = B + C*log10(r) 
+ * @param[in] r        hypocentral distances (km) [n] 
+ *
+ * @param[out] G       forward modeling matrix for PGD [n x 1]
+ *
+ * @result 0 indicates success
+ *
+ */
+int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
+                                       double B, double C,
+                                       double *__restrict__ r,
+                                       double *__restrict__ G)
+{ 
+    const char *fcnm = "GFAST_scaling_PGD__setForwardModel\0";
+    int i;
+    if (n < 1){
+        if (verbose > 0){
+            log_errorF("%s: Invalid number of points: %d\n", fcnm, n);
+        }
+        return -1;
+    }
+    for (i=0; i<n; i++){
+        G[i] = B + C*(log10(r[i]));
+    }
+    return 0;
+}
+//============================================================================//
+/*!
+ * @brief Computes the right hand side in the peak ground displacement 
+ *        estimation s.t. b = log10(d) - A 
+ *
+ * @param[in] n         number of points
+ * @param[in] verbose   controls verbosity (0 is quiet)
+ * @param[in] dist_tol  distance tolerance - if d is less than this then
+ *                      it will be set to a default value (km)
+ * @param[in] dist_def  distance default value (km)
+ * @param[in] d         displacement (cm) [n]
+ * @param[in] A         shift so that b = log10(d) - A
+ *
+ * @param[out] b        right hand side in Gm = b [n]
+ *
+ * @result 0 indicates success
+ *
+ * @author Ben Baker (ISTI)
+ *
+ */
+int GFAST_scaling_PGD__setRHS(int n, int verbose,
+                              double dist_tol, double dist_def,
+                              double A, double *__restrict__ d,
+                              double *__restrict__ b) 
+{
+    const char *fcnm = "GFAST_scaling_PGD__setRHS\0";
+    double dist;
+    int i;
+    if (n < 1){
+        if (verbose > 0){
+            log_errorF("%s: Invalid number of points: %d\n", fcnm, n);
+        }
+        return -1;
+    }
+    for (i=0; i<n; i++){
+        dist = d[i];
+        if (dist - dist_tol < 0.0){dist = dist_def;}
+        b[i] = log10(dist) - A;
+    }
+    return 0;
+}
