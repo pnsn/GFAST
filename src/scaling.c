1349313f2be3500f6b8899c96dc03c3f7ea946fe
Ben Baker <benbaker@isti.com>
2016-04-18 17:12:30 -0700

    Introduced vectorization into scaling

--- src/scaling.c
+++ src/scaling.c
@@ -22,7 +22,7 @@
  *        where \f$ \Delta \f$ is the epicentral distance.
  *
  * @param[in] n         length of arrays 
- * @param[in] verbose   controls verbosity (0 is completely quiet)
+ * @param[in] verbose   controls verbosity (< 2 is quiet)
  * @param[in] dist_tol  displacement tolerance (cm).  if the displacment
  *                      is less than dist_tol it would be set to dist_tol 
  * @param[in] dist_def  displacement default (cm) if d < dist_tol
@@ -50,9 +50,9 @@ int GFAST_scaling_PGD(int n, int verbose,
     const double A = -6.687;
     const double B = 1.500;
     const double C = -0.214;
-    //const double A = -4.434;
-    //const double B = 1.047;
-    //const double C = -0.138;
+    //const double A = -4.434; /* TODO remove with approval */
+    //const double B = 1.047;  /* TODO remove with approval */
+    //const double C = -0.138; /* TODO remove with approval */
     //------------------------------------------------------------------------//
     //
     // Initialize
@@ -66,25 +66,21 @@ int GFAST_scaling_PGD(int n, int verbose,
     WG = NULL;
     // Error check
     if (n < 1){
-        if (verbose > 0){
-            log_errorF("%s: Error invalid number of input stations: %d\n",
-                       fcnm, n);
-        }
+        log_errorF("%s: Error invalid number of input stations: %d\n",
+                   fcnm, n);
         goto ERROR;
     }
     // Get the min epicentral distance 
     repi_min = numpy_min(n, repi);
     repi_min2 = pow(repi_min, 2);
     // Generate the over-determined system: [B + C*log10(r)]*m = log10(d) - A
-    G  = (double *)calloc(n*1, sizeof(double));
-    b  = (double *)calloc(n,   sizeof(double));
+    G  = GFAST_memory_calloc__double(n*1);
+    b  = GFAST_memory_calloc__double(n);
     ierr = GFAST_scaling_PGD__setForwardModel(n, verbose,
                                               B, C, r,
                                               G);
     if (ierr != 0){
-        if (verbose > 0){
-            log_errorF("%s: Error creating G matrix\n", fcnm);
-        }
+        log_errorF("%s: Error creating G matrix\n", fcnm);
         goto ERROR;
     }
     ierr = GFAST_scaling_PGD__setRHS(n, verbose,
@@ -92,14 +88,13 @@ int GFAST_scaling_PGD(int n, int verbose,
                                      A, d,
                                      b);
     if (ierr != 0){
-        if (verbose > 0){
-            log_errorF("%s: Error creating RHS\n", fcnm);
-        }
+        log_errorF("%s: Error creating RHS\n", fcnm);
         goto ERROR;
     }
     // Apply weights
-    WG = (double *)calloc(n, sizeof(double));
-    Wb = (double *)calloc(n, sizeof(double));
+    WG = GFAST_memory_calloc__double(n*1);
+    Wb = GFAST_memory_calloc__double(n);
+    #pragma omp simd
     for (i=0; i<n; i++){
         W = exp(-pow(repi[i], 2)/8.0/repi_min2); // Compute weights
         WG[i] = W*G[i];
@@ -110,18 +105,17 @@ int GFAST_scaling_PGD(int n, int verbose,
                        n, 1, 1, WG, Wb,
                        NULL, M1, NULL, NULL); 
     if (ierr != 0){
-        if (verbose > 0){
-            log_errorF("%s: Error solving the least-squares problem\n", fcnm);
-        }
+        log_errorF("%s: Error solving the least-squares problem\n", fcnm);
         goto ERROR;
     }
     // Compute the estimates: UP = G*M
-    UP = (double *)calloc(n, sizeof(double));
+    UP = GFAST_memory_calloc__double(n);
     cblas_dgemv(CblasColMajor, CblasNoTrans,
                 n, 1, 1.0, G, n, M1, 1, 0.0, UP, 1);
     // Compute the variance reduction
     xnum = 0.0;
     xden = 0.0;
+    #pragma omp simd reduction(+:xnum, xden)
     for (i=0; i<n; i++){
         res = pow(10.0, b[i]) - pow(10.0, UP[i]);
         xnum = xnum + res*res;
@@ -133,16 +127,11 @@ int GFAST_scaling_PGD(int n, int verbose,
     *VR = (1.0 - sqrt(xnum/xden))*100.0;
 ERROR:; // An error was encountered
     // Free space
-    if (b  != NULL){free(b);}
-    if (G  != NULL){free(G);}
-    if (UP != NULL){free(UP);}
-    if (Wb != NULL){free(Wb);}
-    if (WG != NULL){free(WG);}
-    b  = NULL;
-    G  = NULL;
-    UP = NULL;
-    Wb = NULL;
-    WG = NULL;
+    GFAST_memory_free__double(&b);
+    GFAST_memory_free__double(&G);
+    GFAST_memory_free__double(&UP);
+    GFAST_memory_free__double(&Wb);
+    GFAST_memory_free__double(&WG);
     return 0;
 }
 //============================================================================//
@@ -153,7 +142,7 @@ ERROR:; // An error was encountered
  *        and r the hypocentral distances for all stations.
  *
  * @param[in] n        length of r and G (> 0)
- * @param[in] verbose  controls verbosity (0 is quiet)
+ * @param[in] verbose  controls verbosity (< 2 is quiet)
  * @param[in] B        linear shift in G = B + C*log10(r)
  * @param[in] C        scale factor in G = B + C*log10(r) 
  * @param[in] r        hypocentral distances (km) [n] 
@@ -174,11 +163,10 @@ int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
     const char *fcnm = "GFAST_scaling_PGD__setForwardModel\0";
     int i;
     if (n < 1){
-        if (verbose > 0){
-            log_errorF("%s: Invalid number of points: %d\n", fcnm, n);
-        }
+        log_errorF("%s: Invalid number of points: %d\n", fcnm, n);
         return -1;
     }
+    #pragma omp simd
     for (i=0; i<n; i++){
         G[i] = B + C*(log10(r[i]));
     }
@@ -194,8 +182,8 @@ int GFAST_scaling_PGD__setForwardModel(int n, int verbose,
  * @param[in] n         number of points
  * @param[in] verbose   controls verbosity (0 is quiet)
  * @param[in] dist_tol  distance tolerance - if d is less than this then
- *                      it will be set to a default value (km)
- * @param[in] dist_def  distance default value (km)
+ *                      it will be set to a default value (cm)
+ * @param[in] dist_def  distance default value (cm)
  * @param[in] d         displacement (cm) [n]
  * @param[in] A         shift so that b = log10(d) - A
  *
@@ -215,11 +203,10 @@ int GFAST_scaling_PGD__setRHS(int n, int verbose,
     double dist;
     int i;
     if (n < 1){
-        if (verbose > 0){
-            log_errorF("%s: Invalid number of points: %d\n", fcnm, n);
-        }
+        log_errorF("%s: Invalid number of points: %d\n", fcnm, n);
         return -1;
     }
+    #pragma omp simd
     for (i=0; i<n; i++){
         dist = d[i];
         if (dist - dist_tol < 0.0){dist = dist_def;}
