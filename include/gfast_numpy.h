8d90b9dcbf2ace32d3d2409aef7320e2f02e8af5
Ben Baker <benbaker@isti.com>
2016-04-18 17:10:13 -0700

    More function definitions

--- include/gfast_numpy.h
+++ include/gfast_numpy.h
@@ -6,10 +6,14 @@ extern "C"
 #endif
 int numpy_argmax(int n, double *x);
 double numpy_nanmean(int n, double *x, int *iwarn);
+double numpy_nanmax(int n, double *x, int *iwarn);
 double numpy_min(int n, double *x);
 int numpy_lstsq(int mtx_fmt,
                 int m, int n, int nrhs, double *Aref, double *b,
                 double *rcond_in, double *x, int *rank_out, double *svals);
+int numpy_lstsq__qr(int mtx_fmt,
+                    int m, int n, int nrhs, double *Aref, double *b, 
+                    double *x);
 #ifdef __cplusplus
 }
 #endif
