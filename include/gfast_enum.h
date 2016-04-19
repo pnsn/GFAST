0c1c7e54f27b48bcbad173c09facc3211f0d9829
Ben Baker <benbaker@isti.com>
2016-04-18 17:10:01 -0700

    More enums

--- include/gfast_enum.h
+++ include/gfast_enum.h
@@ -5,16 +5,20 @@ enum dtinit_type
 {
     INIT_DT_FROM_DEFAULT = 1,  /*!< Sets GPS sampling period to default */
     INIT_DT_FROM_FILE = 2,     /*!< Obtains GPS sampling period from file */
-    INIT_DT_FROM_TRACEBUF = 3  /*!< Obtains GPS sampling period from 
+    INIT_DT_FROM_TRACEBUF = 3, /*!< Obtains GPS sampling period from 
                                     Earthworm tracebuf */
+    INIT_DT_FROM_SAC = 4       /*!< Obtains GPS sampling period from 
+                                    SAC file */
 };
 
 enum locinit_type
 {
-    INIT_LOCS_FROM_FILE = 1,    /*!< Sets the GPS site locations from 
-                                     SOPAC SECTOR web service file */
-    INIT_LOCS_FROM_TRACEBUF = 2 /*!< Sets the GPS site locations from
-                                     Earthworm tracbuf */
+    INIT_LOCS_FROM_FILE = 1,     /*!< Sets the GPS site locations from 
+                                      SOPAC SECTOR web service file */
+    INIT_LOCS_FROM_TRACEBUF = 2, /*!< Sets the GPS site locations from
+                                      Earthworm tracbuf */
+    INIT_LOCS_FROM_SAC = 3       /*!< Sets the GPS site locations from
+                                      SAC file */
 };
 
 enum opmode_type
