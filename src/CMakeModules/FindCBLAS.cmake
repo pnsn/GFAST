f9448c47b6bf8a6d9faa456e52ec5328986e353b
Ben Baker <benbaker@isti.com>
2016-08-31 16:32:49 -0700

    Trying more verbose method of including blas

--- src/CMakeModules/FindCBLAS.cmake
+++ src/CMakeModules/FindCBLAS.cmake
@@ -1,13 +1,17 @@
 # include(FindLibraryWithDebug)
-if (CBLAS_INCLUDE_DIR AND CBLAS_LIBRARIES)
+if (CBLAS_INCLUDE_DIR AND CBLAS_LIBRARIES AND BLAS_LIBRARIES)
   set(CBLAS_FIND_QUIETLY TRUE)
-endif (CBLAS_INCLUDE_DIR AND CBLAS_LIBRARIES)
+endif (CBLAS_INCLUDE_DIR AND CBLAS_LIBRARIES AND BLAS_LIBRARIES)
 find_path(CBLAS_INCLUDE_DIR
   NAMES cblas.h
   HINTS /usr/include $ENV{CBLASDIR}/include ${INCLUDE_INSTALL_DIR}
 )
 find_library(CBLAS_LIBRARIES
-  NAMES cblas blas
+  NAMES cblas
+  HINTS /usr/lib /usr/lib64 $ENV{CBLASDIR}/lib ${LIB_INSTALL_DIR}
+)
+find_library(BLAS_LIBRARIES
+  NAMES blas
   HINTS /usr/lib /usr/lib64 $ENV{CBLASDIR}/lib ${LIB_INSTALL_DIR}
 )
 #find_file(CBLAS_LIBRARIES
@@ -23,5 +27,5 @@ find_library(CBLAS_LIBRARIES
 #)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CBLAS DEFAULT_MSG
-                                  CBLAS_INCLUDE_DIR CBLAS_LIBRARIES)
-mark_as_advanced(CBLAS_INCLUDE_DIR CBLAS_LIBRARIES)
+                                  CBLAS_INCLUDE_DIR CBLAS_LIBRARIES BLAS_LIBRARIES)
+mark_as_advanced(CBLAS_INCLUDE_DIR CBLAS_LIBRARIES BLAS_LIBRARIES)
