8dfee65a0cd777abb69c926c68382f0e2bd16278
Mike Hagerty <m.hagerty@isti.com>
2020-10-02 10:17:04 -0400

    redebug

--- src/traceBuffer/ewrr/unpackTraceBuf2Messages.c
+++ src/traceBuffer/ewrr/unpackTraceBuf2Messages.c
@@ -341,13 +341,13 @@ LOG_DEBUGMSG("== [unpackTraceBuf t0:%f Third loop over nReadPtr mapping]", ISCL_
                 tb2Data->traces[k].times[kndx+l] = trh->starttime
                                                  + (double) l*dt;
 
-                /*
                 LOG_DEBUGMSG("%s.%s.%s.%s t:%f (npts:%d) (int) data:%d",
                     tb2Data->traces[k].stnm, tb2Data->traces[k].chan,
                     tb2Data->traces[k].netw, tb2Data->traces[k].loc,
                     tb2Data->traces[k].times[kndx+l],
                     npts,
                     tb2Data->traces[k].data[kndx+l]);
+                /*
                 */
             }
             kndx = kndx + npts; 
