f52cbf82c5977c00de803d14402d3538dcf0d46c
Mike Hagerty <m.hagerty@isti.com>
2020-10-12 19:07:01 -0400

    remove test exit

--- src/gfast_eew.c
+++ src/gfast_eew.c
@@ -237,11 +237,11 @@ LOG_MSG("%s: Beginning the acquisition...", fcnm);
         t0 = t1;
         tstatus1 = t0;
 
+/*
         if ((t0 - tbeg) >= 300.) {
 LOG_MSG("== [GFAST t0:%f >= 300 sec since we started --> EXIT CODE", t0);
           goto ERROR;
         }
-/*
 */
 
         //printf("\n== [Iter:%d t0:%f] ==\n", niter,t0);
