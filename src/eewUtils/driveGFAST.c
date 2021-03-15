cc010ed629fb9254b7b6fdd81291934ee7da8b33
Mike Hagerty <m.hagerty@isti.com>
2021-03-15 15:33:05 -0400

    make cmt and ff xml solutions for every iteration when interval=0

--- src/eewUtils/driveGFAST.c
+++ src/eewUtils/driveGFAST.c
@@ -341,7 +341,7 @@ LOG_MSG("driveGFAST: make XML msgs: lpgdSuccess=%d lcmtSuccess=%d lffSuccess=%d\
                 {
                     LOG_ERRMSG("%s", "Error generating PGD XML");
                     if (pgdXML != NULL)
-                    {   
+                    {
                         free(pgdXML);
                         pgdXML = NULL;
                     }
@@ -349,22 +349,21 @@ LOG_MSG("driveGFAST: make XML msgs: lpgdSuccess=%d lcmtSuccess=%d lffSuccess=%d\
                 xmlMessages->pgdXML[xmlMessages->nmessages] = pgdXML;
 
                 //LOG_MSG("Age_of_event=%f [%d] mins %.3f secs", age_of_event, mins, secs);
-                
+
                 if (props.output_interval_mins[0] == 0) { // Output at every iteration
-		      int index = (int)(currentTime - SA.time);
-                      ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "pgd",
-                                               pgdXML, index, false);
+		              int index = (int)(currentTime - SA.time);
+                  ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "pgd", pgdXML, index, false);
                 }
-		else {
-                    for (i=0; i<props.n_intervals; i++){
-                        if (mins == props.output_interval_mins[i] && secs < 1.){
-                          LOG_MSG("Age_of_event=%f --> Output minute %d PGD solution",
+		            else {
+                  for (i=0; i<props.n_intervals; i++){
+                    if (mins == props.output_interval_mins[i] && secs < 1.){
+                      LOG_MSG("Age_of_event=%f --> Output minute %d PGD solution",
                               age_of_event, props.output_interval_mins[i]);
-                          ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "pgd",
-                                                   pgdXML, props.output_interval_mins[i], true);
-                        }
+                      ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "pgd",
+                                               pgdXML, props.output_interval_mins[i], true);
                     }
-		}
+                  }
+		            }
             } //if lpgdSuccess
             // Make the CMT quakeML
             if (lcmtSuccess)
@@ -392,13 +391,21 @@ LOG_MSG("driveGFAST: make XML msgs: lpgdSuccess=%d lcmtSuccess=%d lffSuccess=%d\
                     }
                 }
                 xmlMessages->cmtQML[xmlMessages->nmessages] = cmtQML;
-                for (i=0; i<props.n_intervals; i++){
+                if (props.output_interval_mins[0] == 0) { // Output at every iteration
+		              int index = (int)(currentTime - SA.time);
+                  LOG_MSG("Age_of_event=%f --> Output CMT solution at iter:%d", age_of_event, index);
+                  ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "cmt",
+                                           cmtQML, index, false);
+                }
+                else {
+                  for (i=0; i<props.n_intervals; i++){
                     if (mins == props.output_interval_mins[i] && secs < 1.){
                       LOG_MSG("Age_of_event=%f --> Output minute %d CMT solution",
                           age_of_event, props.output_interval_mins[i]);
                       ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "cmt",
                                                cmtQML, props.output_interval_mins[i], true);
                     }
+                  }
                 }
             }
             // Make the finite fault XML
@@ -443,13 +450,21 @@ LOG_MSG("driveGFAST: make XML msgs: lpgdSuccess=%d lcmtSuccess=%d lffSuccess=%d\
                     }
                 }
                 xmlMessages->ffXML[xmlMessages->nmessages] = ffXML;
-                for (i=0; i<props.n_intervals; i++){
-                    if (mins == props.output_interval_mins[i] && secs < 1.){
-                      LOG_MSG("Age_of_event=%f --> Output minute %d FF solution", 
-                          age_of_event, props.output_interval_mins[i]);
-                      ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "ff",
-                                               ffXML, props.output_interval_mins[i], true);
+                if (props.output_interval_mins[0] == 0) { // Output at every iteration
+		              int index = (int)(currentTime - SA.time);
+                  LOG_MSG("Age_of_event=%f --> Output FF solution at iter:%d", age_of_event, index);
+                  ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "ff",
+                                           ffXML, index, false);
+                }
+                else {
+                  for (i=0; i<props.n_intervals; i++){
+                      if (mins == props.output_interval_mins[i] && secs < 1.){
+                        LOG_MSG("Age_of_event=%f --> Output minute %d FF solution",
+                                 age_of_event, props.output_interval_mins[i]);
+                        ierr = eewUtils_writeXML(props.SAoutputDir, SA.eventid, "ff",
+                                                ffXML, props.output_interval_mins[i], true);
                     }
+                  }
                 }
             }
             xmlMessages->evids[xmlMessages->nmessages]
