51f09dfe08881a9e31052668f8767f27fa852a33
ulbergc <ulbergc@uw.edu>
2021-03-29 17:16:30 -0700

    Add new epoch-calendar functions to timeutils.h

--- include/timeutils.h
+++ include/timeutils.h
@@ -22,6 +22,30 @@ void time_tic(void);
  *  @return time since last time_tic() call
  */
 double time_toc(void);
+
+/*!
+ * @brief Converts the number of seconds since the epoch to the 
+ *        the corresponding year, Julian day, month, day of the month,
+ *        hour, minute, second, and microsecond.  All dates and times
+ *        are in UTC.
+ * 
+ * @result ISCL_SUCCESS indicates success.
+ */
+enum isclError_enum
+    time_epoch2calendar(const double epoch,
+                        int *nzyear, int *nzjday, int *month, int *mday,
+                        int *nzhour, int *nzmin, int *nzsec, int *nzmusec);
+
+/*!
+ * @brief Converts the time given by the year, month, day of month, hour,
+ *        minute, second, and microsecond to seconds since the epoch where
+ *        the epoch is the first day of 1970.  All times and dates are in
+ *        UTC time.
+ * @result Epochal time in UTC seconds since epoch.
+ */ 
+double time_calendar2epoch2(const int nzyear, const int month, const int dom,
+                            const int nzhour, const int nzmin, const int nzsec,
+                            const int nzmusec);
   
 #ifdef __cplusplus
 }
