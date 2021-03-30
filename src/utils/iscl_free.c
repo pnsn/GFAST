48c4d431fa6617de26a17c99a3f62dd3819e70f2
ulbergc <ulbergc@uw.edu>
2021-03-29 17:17:10 -0700

    Comment out unused structs in iscl_free.c

--- src/utils/iscl_free.c
+++ src/utils/iscl_free.c
@@ -171,47 +171,48 @@ void memory_free8l(bool **p)
 #endif
     return;
 }
-//============================================================================//
-/*!
- * @brief Frees a zpk filter structure.
- * @param[in,out] zpk   On input this is a zpk structure.
- * @param[in,out] zpk   On exit the zpk structure memory is released and
- *                      variables set to 0.
- * @ingroup memory_free
- */
-void memory_freeZPK(struct signalZPK_struct *zpk)
-{
-    memory_free64z(&zpk->p);
-    memory_free64z(&zpk->z);
-    memset(zpk, 0, sizeof(struct signalZPK_struct));
-    return; 
-}
-/*!
- * @brief Frees a ba filter structure.
- * @param[in,out] ba   On input this is a ba structure.
- * @param[in,out] ba   On exit the ba structure memory is released and
- *                     variables set to 0.
- * @ingroup memory_free
- */
-void memory_freeBA(struct signalBA_struct *ba)
-{
-    memory_free64f(&ba->b);
-    memory_free64f(&ba->a);
-    memset(ba, 0, sizeof(struct signalBA_struct));
-}
-/*!
- * @brief Frees a SOS filter structure.
- * @param[in,out] sos  On input this is a sos structure.
- * @param[in,out] sos  On exit the ba structure memory is released and
- *                     variables set to 0.
- * @ingroup memory_free
- */
-void memory_freeSOS(struct signalSOS_struct *sos)
-{
-    memory_free64f(&sos->b);
-    memory_free64f(&sos->a);
-    memset(sos, 0, sizeof(struct signalSOS_struct));
-}
+// Comment out because not used - CWU 3/29/21
+// //============================================================================//
+// /*!
+//  * @brief Frees a zpk filter structure.
+//  * @param[in,out] zpk   On input this is a zpk structure.
+//  * @param[in,out] zpk   On exit the zpk structure memory is released and
+//  *                      variables set to 0.
+//  * @ingroup memory_free
+//  */
+// void memory_freeZPK(struct signalZPK_struct *zpk)
+// {
+//     memory_free64z(&zpk->p);
+//     memory_free64z(&zpk->z);
+//     memset(zpk, 0, sizeof(struct signalZPK_struct));
+//     return; 
+// }
+// /*!
+//  * @brief Frees a ba filter structure.
+//  * @param[in,out] ba   On input this is a ba structure.
+//  * @param[in,out] ba   On exit the ba structure memory is released and
+//  *                     variables set to 0.
+//  * @ingroup memory_free
+//  */
+// void memory_freeBA(struct signalBA_struct *ba)
+// {
+//     memory_free64f(&ba->b);
+//     memory_free64f(&ba->a);
+//     memset(ba, 0, sizeof(struct signalBA_struct));
+// }
+// /*!
+//  * @brief Frees a SOS filter structure.
+//  * @param[in,out] sos  On input this is a sos structure.
+//  * @param[in,out] sos  On exit the ba structure memory is released and
+//  *                     variables set to 0.
+//  * @ingroup memory_free
+//  */
+// void memory_freeSOS(struct signalSOS_struct *sos)
+// {
+//     memory_free64f(&sos->b);
+//     memory_free64f(&sos->a);
+//     memset(sos, 0, sizeof(struct signalSOS_struct));
+// }
 //============================================================================//
 /*!
  * @brief Frees a float pointer and sets the pointer to NULL.
