2a5c00249c02f2ee75158c8f8aa2f0fc2b06b7e2
Mike Hagerty <m.hagerty@isti.com>
2021-05-11 14:03:53 -0400

    tweak

--- src/uw/gfast2web.c
+++ src/uw/gfast2web.c
@@ -135,7 +135,9 @@ int main(int argc, char *argv[])
             break;
 
           case 'i':
-            output_interval = atoi(&argv[i][1]);
+            printf("scan int from argv[i+1]=%s\n", argv[i+1]);
+            //output_interval = atoi(&argv[i][2]);
+            output_interval = atoi(&argv[i+1]);
             printf("-i: Output i:[%d] interval\n", output_interval);
             break;
 
