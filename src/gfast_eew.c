94717ab22830617301c2c6ee560733401d5d1536
Mike Hagerty <m.hagerty@isti.com>
2021-05-18 12:30:12 -0400

    fix missing }

--- src/gfast_eew.c
+++ src/gfast_eew.c
@@ -344,6 +344,7 @@ for (i=0;i<tb2Data.ntraces;i++){
     //for (j=0; j<tb2Data.traces[i].npts; j++){
           printf("  tb2Data chunk:%d i1:%d i2:%d is:%d t:%f val:%f\n", 
               chunk, i1, i2, is, tb2Data.traces[i].times[is], tb2Data.traces[i].data[is]);
+      }
     }
 }
 //exit(0);
