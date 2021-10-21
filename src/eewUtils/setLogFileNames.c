2fba1386723c52817acedff7312229edf9735104
ulbergc <ulbergc@uw.edu>
2021-10-21 11:23:02 -0700

    Fix log file name bug related to adding configurable output dir

--- src/eewUtils/setLogFileNames.c
+++ src/eewUtils/setLogFileNames.c
@@ -12,36 +12,40 @@
  * @param [out] debugLogFileName
  * @param [out] warnLogFileName
  */
-void eewUtils_setLogFileNames(const char *eventid,const char *outputDir,
-			      char errorLogFileName[PATH_MAX],
-			      char infoLogFileName[PATH_MAX],
-			      char debugLogFileName[PATH_MAX],
+void eewUtils_setLogFileNames(const char *eventid,
+                              const char *outputDir,
+                              char errorLogFileName[PATH_MAX],
+                              char infoLogFileName[PATH_MAX],
+                              char debugLogFileName[PATH_MAX],
                               char warnLogFileName[PATH_MAX])
 {
   // Set the log file names
-  memset(errorLogFileName, 0, PATH_MAX*sizeof(char));
+  char fileNameBase[PATH_MAX];
+  memset(fileNameBase, 0, PATH_MAX*sizeof(char));
   if (outputDir == NULL) {
-    strcpy(errorLogFileName, "./\0");
-  }
-  else {
-    strcpy(errorLogFileName, outputDir);
+    strcpy(fileNameBase, "./\0");
+  } else {
+    strcpy(fileNameBase, outputDir);
     if (outputDir[strlen(outputDir)-1] != '/') {
-      strcat(errorLogFileName, "/\0");
+      strcat(fileNameBase, "/\0");
     }
   }
-  strcat(errorLogFileName, eventid);
+  strcat(fileNameBase, eventid);
+
+  memset(errorLogFileName, 0, PATH_MAX*sizeof(char));
+  strcpy(errorLogFileName, fileNameBase);
   strcat(errorLogFileName, "_error.log\0");
   
   memset(infoLogFileName, 0, PATH_MAX*sizeof(char));
-  strcpy(infoLogFileName, errorLogFileName);  // other files start with same root
+  strcpy(infoLogFileName, fileNameBase);
   strcat(infoLogFileName, "_info.log\0");
   
   memset(debugLogFileName, 0, PATH_MAX*sizeof(char));
-  strcpy(debugLogFileName, errorLogFileName);
+  strcpy(debugLogFileName, fileNameBase);
   strcat(debugLogFileName, "_debug.log\0");
   
   memset(warnLogFileName, 0, PATH_MAX*sizeof(char));
-  strcpy(warnLogFileName, errorLogFileName);
+  strcpy(warnLogFileName, fileNameBase);
   strcat(warnLogFileName, "_debug.log\0");
   
   return;
