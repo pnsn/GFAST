a74748ece3bd26b617fe9b825aec3d62dc490da1
ulbergc <ulbergc@uw.edu>
2021-10-27 15:26:41 -0700

    Update gfast_config.h

--- include/gfast_config.h
+++ include/gfast_config.h
@@ -1,6 +1,6 @@
 #ifndef _gfast_config__h_
 #define _gfast_config__h_ 1
-#define GFAST_ALGORITHM_VERSION "0.01"
+#define GFAST_VERSION "1.0.0"
 #define GFAST_INSTANCE "PNSN"
 #define GFAST_MAXMSG_LEN 4096
 #define MAX_OUTPUT_INTERVALS 16
