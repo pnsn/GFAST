cde952c0ee98070354bc82d0a28028f2e0bfc166
ulbergc <ulbergc@uw.edu>
2021-11-08 17:26:22 -0800

    Update version number for make and CMake systems

--- include/gfast_config.h
+++ include/gfast_config.h
@@ -1,6 +1,6 @@
 #ifndef _gfast_config__h_
 #define _gfast_config__h_ 1
-#define GFAST_VERSION "1.1.0-2021-11-03"
+#define GFAST_VERSION "1.1.1-2021-11-08"
 #define GFAST_INSTANCE "PNSN"
 #define GFAST_MAXMSG_LEN 4096
 #define MAX_OUTPUT_INTERVALS 16
